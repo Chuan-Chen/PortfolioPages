d908768f710d856e5cd37f3e9286f29f66aee322
Chuan Chen <chuan.chen402@qmail.cuny.edu>
2023-12-23 16:07:30 -0500

    update

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,14 +1,12 @@
 import React from "react";
 import "./Intro.css";
 import Resume from "../files/Chuan_Chen_Resume.pdf"
-
-
+import Profile_img from "../images/Profile.jpg";
 
 function Profile(props){
-
     return (
-        <div>
-
+        <div style = {{display: "grid", height: 200}}>
+            <img src = {Profile_img} height = {150} style={{borderRadius: "100%", justifySelf: "center", boxShadow: "0 0 5px black"}}></img>
         </div>
     )
 }
@@ -18,10 +16,19 @@ export default function Intro(props){
     return (
         <div className = "intro">
             <Profile></Profile>
-            <div><a href = {Resume} alt = "Resume" >Resume</a></div>
-            <div>Phone: (917)605-6373</div>
-            <div>Email: <a href = "mailto:chuan.chen.info@gmail.com">chuan.chen.info@gmail.com</a></div>
-            <div>About: Hi my name is Chuan Chen</div>
+            <div>Resume📄: <a href = {Resume} alt = "Resume" target = "_blank">Resume</a></div>
+            <div>Phone📞: (917)605-6373</div>
+            <div>Email📧: <a href = "mailto:chuan.chen.info@gmail.com">chuan.chen.info@gmail.com</a></div>
+            <br></br>
+            <div style = {{width: "400px"}}>About✍️: Hi my name is Chuan Chen, an experienced C++ Tutor with a demonstrated history of working in the higher education. Skilled in Sales, Visual and Intercultural Communication, JavaScript, NodeJS, HTML, CSS, C++, Java, REST API and MongoDB.</div>
+            <br></br>
+            <div>Projects: 
+                <li><a href = "https://shop.nauhc.dev/" target = "_blank">DeShop</a></li>
+                <li><a href = "https://chuan-chen.github.io/Fullstack-Projects/IP/index.html" target = "_blank">IPFinder</a></li>
+                <li><a href = "https://chuan-chen.github.io/Fullstack-Projects/Weather/dist/index.html" target = "_blank">Weather App</a></li>
+                <li><a href = "https://chuan-chen.github.io/Fullstack-Projects/Tic_Tac_Toe/index.html" target = "_blank">Tic Tac Toe</a></li>
+            </div>
+            <div style={{fontSize: "11px"}}><br></br>Work in progress...</div>
         </div>
     )
 }
