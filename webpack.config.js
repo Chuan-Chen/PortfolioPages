af7cd3c6fef11bff2d8dc0cd71adee3e45b72e9c
Chuan Chen <chuan.chen402@qmail.cuny.edu>
2023-12-23 17:54:58 -0500

    update

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,7 @@ module.exports = {
             use: ["style-loader","css-loader"],
         },
         {
-            test: /\.(png|jpe?g|gif)$/i,
+            test: /\.(png|jpe?g|gif|pdf)$/i,
             use: [
               {
                 loader: 'file-loader',
