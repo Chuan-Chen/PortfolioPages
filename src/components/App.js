61b437cbe153c61da415b97bd8ae219bd68a6f01
Chuan Chen <chuan.chen402@qmail.cuny.edu>
2024-05-26 19:30:04 -0400

    minor updates

--- src/components/App.js
+++ src/components/App.js
@@ -2,17 +2,24 @@ import React from "react";
 import Intro from "./Intro";
 import Footer from "./Footer";
 import Header from "./Header";
+import Scene from "../threejs/Box.js";
 import "./App.css";
 
 export default function App(){
 
-    return (
-        <div className = "container">
+/**
+ *         <div className = "container">
             <div className = "center">
             <Header/>
             <Intro/>
             <Footer/>
             </div>
+        </div>
+ */
+
+    return (
+        <div style = {{height: "100vh", width: "100%"}}>
+        <Scene></Scene>
         </div>
 
     )
